be222ef97eade46887c15421e4e6756c6f7576c7
FishingCat <1973949956@qq.com>
2020-06-16 13:27:14 +0800

    第一次提交

---
+++ Hello World.py
@@ -0,0 +1,3 @@
+#!/user/bin/python
+# Filename : helloworld.py
+print ("hello world")  
---
+++ 猜数字.py
@@ -0,0 +1,22 @@
+'''
+MYFAV = 3
+num_of_options = input("请问您有几个选项：")
+result = MYFAV
+if MYFAV > int(num_of_options):
+    result = 1
+else:
+    result = MYFAV
+elif:
+    pass
+print("您的最佳选项：", result)
+'''
+
+'''
+import time
+time.time()
+int(time.time())
+print(time.time() * 100 % 5 + 1)   #靠时间戳随机
+'''
+a = '红烧鱼'
+hash(a)
+print(hash(a))
